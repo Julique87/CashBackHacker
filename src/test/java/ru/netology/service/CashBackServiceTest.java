b2efb07f81bee158831fc265e0b1b0879baa9eb3
Julia Novikova <julikova.n@gmail.com>
2023-06-21 23:52:48 +0300

    add branch junit4

--- src/test/java/ru/netology/service/CashBackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackServiceTest.java
@@ -1,61 +1,61 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashBackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemain() {                               // Работа сервиса в классе эквивалетности до 1000 руб.
         CashBackService service = new CashBackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldNotRemainIfAmountAbove1000() {               // Класс эквавалентности > 1000
         CashBackService service = new CashBackService();
         int amount = 1200;
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldNotRemainWhenBoundary() {                  // Граничное значение
         CashBackService service = new CashBackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainWhenBelowBoundary() {                 // Ниже граничного значения на 1 позицию
         CashBackService service = new CashBackService();
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
 
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldNotRemainWhenUpperBoundary() {              // Выше граничного значения на 1 позицию
         CashBackService service = new CashBackService();
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
 
     }
 
